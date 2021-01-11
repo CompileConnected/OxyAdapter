21e0495d6e59855247204094d016301a0d879862
Yohanes <yokho95@gmail.com>
2021-01-12 00:52:09 +0700

    remove factory because unnecessary

--- oxyadapter/src/main/java/com/yohanes/oxyadapter/core/ViewHolderModel.kt
+++ oxyadapter/src/main/java/com/yohanes/oxyadapter/core/ViewHolderModel.kt
@@ -2,18 +2,6 @@ package com.yohanes.oxyadapter.core
 
 import android.view.View
 
-@JvmName("holderFactory2")
-inline fun <VH : OxyViewHolder, reified VHM : ViewHolderModel<VH>> holderFactory(
-    init: ViewHolderModel.Builder<VH, VHM>.() -> Unit
-) = ViewHolderModel.Builder<VH, VHM>().init()
-
-
-@JvmName("holderFactory1")
-inline fun <reified VHM : ViewHolderModel<OxyViewHolder>> holderFactory(
-    init: ViewHolderModel.Builder<OxyViewHolder, VHM>.() -> Unit
-) = ViewHolderModel.Builder<OxyViewHolder, VHM>().init()
-
-
 abstract class ViewHolderModel<VH : OxyViewHolder> {
 
     abstract val layoutId: Int
@@ -24,35 +12,7 @@ abstract class ViewHolderModel<VH : OxyViewHolder> {
         return OxyViewHolder(itemView) as VH
     }
 
-    class Builder<VH : OxyViewHolder, VHM : ViewHolderModel<VH>> {
-
-        private var externalBinder: ExternalBindViewHolder<VH, ViewHolderModel<VH>>? = null
-        private var layoutId: Int? = null
-
-        fun bind(b: (holder: VH, viewHolderModel: VHM) -> Unit) {
-            val s: ExternalBindViewHolder<VH, ViewHolderModel<VH>> =
-                object : ExternalBindViewHolder<VH, ViewHolderModel<VH>> {
-                    override fun onBind(holder: VH, viewHolderModel: ViewHolderModel<VH>) {
-                        b.invoke(holder, viewHolderModel as VHM)
-                    }
-                }
-            this.externalBinder = s
-        }
-
-        fun setLayoutId(layoutId: Int) {
-            this.layoutId = layoutId
-        }
-
-        fun build() = object : ViewHolderModel<VH>() {
-            override val layoutId: Int
-                get() = this@Builder.layoutId!!
-
-        }.apply {
-            externalBinder = this@Builder.externalBinder
-        }
-    }
-
-    fun setBind(b: (holder: VH, viewHolderModel: ViewHolderModel<VH>) -> Unit) {
+    fun bind(b: (holder: VH, viewHolderModel: ViewHolderModel<VH>) -> Unit) {
         val s: ExternalBindViewHolder<VH, ViewHolderModel<VH>> =
             object : ExternalBindViewHolder<VH, ViewHolderModel<VH>> {
                 override fun onBind(holder: VH, viewHolderModel: ViewHolderModel<VH>) {
--- oxyadapter/src/main/java/com/yohanes/oxyadapter/databinding/ViewBindingHolderModel.kt
+++ oxyadapter/src/main/java/com/yohanes/oxyadapter/databinding/ViewBindingHolderModel.kt
@@ -4,16 +4,6 @@ import android.view.View
 import androidx.viewbinding.ViewBinding
 import com.yohanes.oxyadapter.core.ViewHolderModel
 
-@JvmName("bindHolderFactory2")
-inline fun <VB : ViewBinding, reified VHM : ViewHolderModel<OxyViewHolder<VB>>> bindHolderFactory(
-    init: ViewHolderModel.Builder<OxyViewHolder<VB>, VHM>.() -> Unit
-) = ViewHolderModel.Builder<OxyViewHolder<VB>, VHM>().init()
-
-@JvmName("bindHolderFactory1")
-inline fun <reified VHM : ViewHolderModel<OxyViewHolder<*>>> bindHolderFactory(
-    init: ViewHolderModel.Builder<OxyViewHolder<*>, VHM>.() -> Unit
-) = ViewHolderModel.Builder<OxyViewHolder<*>, VHM>().init()
-
 abstract class ViewBindingHolderModel<T : ViewBinding> : ViewHolderModel<OxyViewHolder<T>>() {
 
     protected abstract fun initViewBinding(itemView: View): T
